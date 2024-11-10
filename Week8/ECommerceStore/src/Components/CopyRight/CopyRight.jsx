25ea6061be92c7070e98c55bdd628e2e9e949dd2
shreyas <shreyas.nayak.003@gmail.com>
2024-11-10 12:02:37 +0530

    changes

--- Week8/ECommerceStore/src/Components/CopyRight/CopyRight.jsx
+++ Week8/ECommerceStore/src/Components/CopyRight/CopyRight.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 
 const CopyRight = (props) => {
     return (
-        <a href='https://github.com/debmalya2003?tab=repositories' target='_blank' rel='noreferrer' >
+        <a href='' target='_blank' rel='noreferrer' >
 
             <Typography variant="body1" fontWeight="bold" color="text.secondary" align="center" {...props} style={{ color: '#1976d2',  }}>
                 {' '}
                 {new Date().getFullYear()}
                 {/* {'.'} */}
                 {' © '}
-                Developed By Debmalya Mukherjee
+                Developed By Shreyas Nayak
             </Typography>
         </a>
     )
